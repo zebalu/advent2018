c96a487b933c71d888e01f5c9fccad1f50e2085a
zebalu <zebalu@gmail.com>
2018-12-03 22:44:10 +0100

    puzzle 1 and 2

--- day03puzzle1/src/main/groovy/day3/Square.groovy
+++ day03puzzle1/src/main/groovy/day3/Square.groovy
@@ -1,5 +1,10 @@
 package day3
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@EqualsAndHashCode
+@ToString(includes=['topLeftX', 'topLeftY', 'width', 'height'])
 class Square {
     
     int topLeftX
@@ -44,14 +49,31 @@ class Square {
     
     Square minus(Square other) {
         Square result = new Square(-1, -1, 0, 0)
-        if(doOverlap(topLeftX, topRightX, other.topLeftX, other.topRightX)) {
-            
+        if(doOverlap(topLeftX, topRightX, other.topLeftX, other.topRightX) &&
+            doOverlap(topLeftY, bottomLeftY, other.topLeftY, other.bottomLeftY)) {
+            Tuple2<Integer, Integer> xes = getCommmons(topLeftX, topRightX, other.topLeftX, other.topRightX)
+            Tuple2<Integer, Integer> ys = getCommmons(topLeftY, bottomLeftY, other.topLeftY, other.bottomLeftY)
+            result.topLeftX=xes.first
+            result.width=xes.second-xes.first
+            result.topLeftY=ys.first
+            result.height=ys.second-ys.first
         }
         return result
     }
     
+    boolean contains(x, y) {
+        return topLeftX <= x && x < topRightX &&
+               topLeftY <= y && y < bottomLeftY
+    }
+    
     private static boolean doOverlap(a1, a2, b1, b2) {
-        return a1<b1 && a2>b1 || b1<a1 && b2 > a1
+        return a1<=b1 && b1<a2 || b1<=a1 && a1 < b2
+    }
+    
+    private static Tuple2<Integer, Integer> getCommmons(a1, a2, b1, b2) {
+        int start = Math.max(a1, b1)
+        int end = Math.min(a2, b2)
+        return new Tuple2(start, end)
     }
     
     static Tuple2<Integer, Square> fromString(String idSquare) {
@@ -62,6 +84,17 @@ class Square {
         String[] dimensionParts = squareParts[1].split('x')
         def square = new Square(cornerParts[0] as Integer, cornerParts[1] as Integer, dimensionParts[0] as Integer, dimensionParts[1] as Integer)
         return new Tuple2(id, square)
+    }
+    
+    static Map<Integer, Square> claims(InputStream is) {
+        Map<Integer, Square> claims = [:]
+        is.withReader { r->
+            r.eachLine { l->
+                def idS = fromString(l)
+                claims[idS.first] = idS.second
+            }
+        }
+        return claims
     }
 
 }
--- day03puzzle1/src/test/groovy/day3/SquareTest.groovy
+++ day03puzzle1/src/test/groovy/day3/SquareTest.groovy
@@ -1,30 +1,32 @@
 package day3;
 
+import java.util.Map.Entry
+
 import spock.lang.Specification
 
 class SquareTest extends Specification {
 
     def "area  works"() {
         Square sq = new Square(0, 0, 10, 8)
-        
+
         expect:
         80 == sq.area()
     }
-    
+
     def 'parsing works' () {
         def result = Square.fromString('#3 @ 12,32: 10x2')
-        
+
         expect:
         result.first == 3 &&
-        result.second.topLeftX == 12 &&
-        result.second.topLeftY == 32 &&
-        result.second.width == 10 &&
-        result.second.height == 2 
+                result.second.topLeftX == 12 &&
+                result.second.topLeftY == 32 &&
+                result.second.width == 10 &&
+                result.second.height == 2
     }
-    
+
     def "corners work"() {
         Square sq = new Square(1, 2, 10, 8)
-        
+
         expect:
         1 == sq.topLeftX
         2 == sq.topLeftY
@@ -34,5 +36,83 @@ class SquareTest extends Specification {
         10 == sq.bottomLeftY
         11 == sq.bottomRightX
         10 == sq.bottomRightY
+    }
+
+    def "non overlaping has 0 area"() {
+        Square s1 = new Square(0, 0, 3, 3)
+        Square s2 = new Square(3, 3, 2, 2)
+
+        expect:
+        0 == (s1 - s2).area()
+    }
+
+    def "overlaping calculated correctly"() {
+        Square s1 = new Square(0, 0, 4, 4)
+        Square s2 = new Square(2, 2, 3, 3)
+
+        expect:
+        new Square(2, 2, 2, 2) == s1 - s2
+    }
+    /*
+     def "get common area of input"() {
+     int areaSum = 0
+     List<Square> squares = []
+     getClass().getResourceAsStream("/input5.txt").withReader { r->
+     r.eachLine { 
+     squares += Square.fromString(it).second
+     }
+     }
+     for(int x=0; x<1000; x+=1) {
+     for(int y=0; y<1000; y+=1) {
+     int inSquares = 0
+     for(int i=0; i<squares.size() && inSquares < 2; ++i) {
+     inSquares += squares[i].contains(x, y)?1:0
+     if(squares[i].contains(x, y) && inSquares > 1) {
+     println "$x, $y is in: $i= ${squares[i]}, was found in: $inSquares"
+     }
+     }
+     if(inSquares > 1) {
+     ++areaSum
+     println "area: $areaSum"
+     }
+     }
+     }
+     expect:
+     areaSum == 111630
+     }*/
+
+    def "points found"(int x, int y, boolean contains) {
+        Square s = new Square(0, 0, 2, 2)
+
+        expect:
+        contains == s.contains(x, y)
+
+        where:
+        x| y| contains
+        0| 0| true
+        0| 2| false
+        1| 1| true
+        3| 1| false
+        2| 3| false
+    }
+
+    def "get not overlaping square"() {
+        Map<Integer, Square> claims = Square.claims(getClass().getResourceAsStream("/input5.txt"))
+        List<Entry<Integer, Square>> entries = new ArrayList<>(claims.entrySet())
+        Integer id = null;
+        for(int i=0; i<entries.size() && id == null; ++i) {
+            def overlapFound = false
+            for(int j=0; j<entries.size() && !overlapFound; ++j) {
+                if(i!=j) {
+                    overlapFound = ((entries[i].value - entries[j].value).area() > 0) || ((entries[j].value - entries[i].value).area() > 0)
+                }
+            }
+            if(!overlapFound) {
+                id = entries[i].key
+            }
+        }
+
+        expect:
+        id == 724
     }
 }
