5354a6008e99eba84529cf0ddc86c6f6df7d94a7
zebalu <zebalu@gmail.com>
2018-12-07 23:49:00 +0100

    make it faster

--- day02puzzle2/src/main/groovy/day02/CheckSumBase.groovy
+++ day02puzzle2/src/main/groovy/day02/CheckSumBase.groovy
@@ -6,13 +6,9 @@ import groovy.transform.Canonical
 class CheckSumBase {
 
     final String id
-    final boolean hasDoubleLetter
-    final boolean hasTripletLetter
     
     CheckSumBase(String id) {
         this.id=id
-        hasDoubleLetter=findDoubletts()
-        hasTripletLetter=findTripletts()
     }
     
     int size() {
@@ -28,30 +24,6 @@ class CheckSumBase {
             }
         }
         return new CheckSumBase(sb.toString())
-    }
-    
-    private boolean findDoubletts() {
-        return findLettersRepeating(2)
-    }
-    
-    private boolean findTripletts() {
-        return findLettersRepeating(3)
-    }
-    
-    private boolean findLettersRepeating(times) {
-        def found = false
-        id.each { check ->
-            def count = 0
-            id.each { letter ->
-                if(letter == check) {
-                    ++count
-                }
-            }
-            if(count == times) {
-                found = true
-            }        
-        } 
-        return found
     }
     
 }
--- day02puzzle2/src/test/groovy/day02/CheckSumBaseTest.groovy
+++ day02puzzle2/src/test/groovy/day02/CheckSumBaseTest.groovy
@@ -4,21 +4,5 @@ import spock.lang.Specification
 
 class CheckSumBaseTest extends Specification {
 
-    def "input counts"(String id, boolean hasDoubleLetters, boolean hasTripleLetters) {
-        CheckSumBase csb = new CheckSumBase(id)
-        
-        expect:
-        hasDoubleLetters == csb.hasDoubleLetter
-        hasTripleLetters == csb.hasTripletLetter
-        
-        where:
-        id      |hasDoubleLetters|hasTripleLetters
-        'bababc'|true            |true
-        'abbcde'|true            |false
-        'abcccd'|false           |true
-        'aabcdd'|true            |false
-        'abcdee'|true            |false
-        'ababab'|false           |true
-    }
     
 }
